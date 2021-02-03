97fa2a96df0dc1b9047644ba8fb2e057c33fea9c
dan-primo <danielly.primo@gmail.com>
2021-02-03 09:43:53 -0300

    aula03-part1

--- scripts.js
+++ scripts.js
@@ -1,39 +1,35 @@
 const Modal = {
-    //Desafio: Esse método pode ser substituido por uma função toggele
-    open() {
-      document.querySelector(".modal-overlay")
-      .classList
-      .add("active");
-    },
-    close() {
-      document.querySelector(".modal-overlay")
-      .classList
-      .remove("active");
-    },
-  };
-  
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    }
-  ]
+  //Desafio: Esse método pode ser substituido por uma função toggele
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
 
-    /* Organizando a linha de raciocínio:
+const transactions = [
+  {
+    id: 1,
+    description: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
+  },
+  {
+    id: 2,
+    description: "Website",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    id: 3,
+    description: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+];
+
+/* Organizando a linha de raciocínio:
         1 - Preciso somar as entradas
         2 - Em seguida, somar as saídas e,
         3 - Remover das entradas o valor das saídas
@@ -41,41 +37,83 @@ const transactions = [
 
     */
 const Transaction = {
-      incomes() {
-        // somar as entradas
-      },
-      expenses() {
-          // somar as saídas
-      },
-      total() {
-          // entradas - saídas
-      }
+  incomes() {
+    let income = 0;
+    // somar as entradas
+    // pegar todas as transações
+    // se for maior que zero
+    // somar a uma variavel e retornar a variavel
+    return income;
+  },
+  //break 1:20h
+  expenses() {
+    // somar as saídas
+  },
+  total() {
+    // entradas - saídas
+  },
+};
 
-  }
-
-  /* Organizando a linha de raciocínio:
+/* Organizando a linha de raciocínio:
     Substituir os dados do HTML com os dados do JS
   */
 
 const DOM = {
-    addTransaction(transaction, index) {
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction()
-    },
+  transactionsContainer: document.querySelector("#data-table tbody"),
+  addTransaction(transaction, index) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
 
-    innerHTMLTransaction() {
+    DOM.transactionsContainer.appendChild(tr);
+  },
 
-        const html = `
-        <td class="description">Luz</td>
-        <td class="expense">- R$ 500,00</td>
-        <td class="date">23/01/2021</td>
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
+    const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
           <img src="./assets/minus.svg" 
           alt="Remover Transação" />
         </td>
-        `
-        return html
-    }
-}
+        `;
+    return html;
+  },
 
-// 46min
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Transaction.incomes();
+    document.getElementById(
+      "expenseDisplay"
+    ).innerHTML = Transaction.expenses();
+    document.getElementById("totalDisplay").innerHTML = Transaction.total();
+  },
+};
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+    // ternario de verdadeiro ou falso
+
+    value = String(value).replace(/\D/g, "");
+    //regex - expressão regular
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return signal + value;
+  },
+};
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction);
+});
+
+DOM.updateBalance();
